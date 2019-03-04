1d480134173bc38f3639800cd25a9ecf829005ed
ahmed <ahmedbhna@gmail.com>
2019-03-04 18:12:24 +0400

    Update transfer_learning.py

--- transfer_learning.py
+++ transfer_learning.py
@@ -114,3 +114,15 @@ class Resnet_Added_Layers_Half_Frozen(nn.Module):
         x = self.projective2(x)
         return x
       
+class Regress_Loss(torch.nn.Module):
+    
+    def __init__(self):
+        super(Regress_Loss,self).__init__()
+        
+    def forward(self,x,y):
+        y_shape = y.size()[1]
+        x_added_dim = x.unsqueeze(1)
+        x_stacked_along_dimension1 = x_added_dim.repeat(1,NUM_WORDS,1)
+        diff = torch.sum((y - x_stacked_along_dimension1)**2,2)
+        totloss = torch.sum(torch.sum(torch.sum(diff)))
+        return totloss
